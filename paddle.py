3092642db357b9da4304b932bce31514a0cf029d
Thoth19 <kpseipp@gmail.com>
2014-12-16 13:35:58 -0800

    paddle is black line, oriented correctly

--- paddle.py
+++ paddle.py
@@ -1,13 +1,14 @@
 import math, os, pygame
-paddles = []
 class PaddleSprite(pygame.sprite.Sprite):
     '''
     This is the user controlled paddle in a game of Pong
     '''
     def __init__(self, position):
         pygame.sprite.Sprite.__init__(self)
-        paddles.append(self)
-        self.rect=pygame.Rect(position[0], position [1], 16, 128)
+        self.image = pygame.Surface([128,16])
+        self.rect = self.image.get_rect()
+        self.rect.x=position[0]
+        self.rect.y=position[1]
     def move(self, position):
         ''' moves to given position'''
         self.rect=pygame.Rect(position[0], position [1], 16, 128)
