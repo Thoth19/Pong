4fa63a3f6736a58b86948a685a2366c1bdf9a1e8
Thoth19 <kpseipp@gmail.com>
2014-12-17 14:42:10 -0800

    Is the wall class. Forgot to add earlier. it sits there to be bounced off of by ball

---
+++ wall.py
@@ -0,0 +1,10 @@
+import pygame, os, math
+class WallSprite(pygame.sprite.Sprite):
+    def __init__(self, position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([16,16])
+        self.image.fill((0,0,255))
+        self.rect = self.image.get_rect()
+        self.rect.x = position[0]
+        self.rect.y = position[1]
+        
