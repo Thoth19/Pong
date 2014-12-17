36abbe40f69d7b7f839438f9d26db76425edecd3
Thoth19 <kpseipp@gmail.com>
2014-12-17 14:48:09 -0800

    comments

--- ball.py
+++ ball.py
@@ -12,21 +12,32 @@ class BallSprite(pygame.sprite.Sprite):
         self.image = pygame.Surface([16,16])
         self.image.fill((0,0,0))
         # self.image.set_colorkey((0,0,0))
-        pygame.draw.circle(self.image,(255,255,255), position, 8)
+        # pygame.draw.circle(self.image,(0,255,0), position, 8)
+        # Unclear why making it a circle doesn't work. 
         self.rect = self.image.get_rect()
         self.rect.x = position[0]
         self.rect.y = position[1]
         self.velocity = abs(velocity[0]),abs(velocity[1])
+        # velocity is actually speed
         self.vel_dir = copysign(1,velocity[0]),copysign(1,velocity[1])
     def update(self):
+        '''
+        moves ball based on velocity and keeps velocity below maximum
+        '''
         self.velocity = min(self.velocity[0],MAX_SPEED),min(self.velocity[1],MAX_SPEED)
-        # self.image=pygame.transform.rotate(self.src_image,0)
+        # velocity cannot go above maximum
         self.rect.x += self.velocity[0] * self.vel_dir[0]
         self.rect.y += self.velocity[1] * self.vel_dir[1]
     def x_dir(self):
+        '''
+        Changes direction of x velocity
+        '''
         self.vel_dir = self.vel_dir[0] * -1, self.vel_dir[1]
         self.velocity = self.velocity[0] + 1, self.velocity[1]
     def y_dir(self):
+        '''
+        Changes direction of y velocity
+        '''
         self.vel_dir = self.vel_dir[0], self.vel_dir[1] * -1
         self.velocity = self.velocity[0], self.velocity[1] + 1
 
