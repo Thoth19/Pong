75796d8bee794997d90a3b3e9157ac844f6cca5f
Thoth19 <kpseipp@gmail.com>
2014-12-17 15:02:42 -0800

    renoved whitespace

--- wall.py
+++ wall.py
@@ -1,5 +1,6 @@
 import pygame, os, math
 class WallSprite(pygame.sprite.Sprite):
+    ''' This class is a 16 by 16 blue block to serve as the border of game'''
     def __init__(self, position):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([16,16])
